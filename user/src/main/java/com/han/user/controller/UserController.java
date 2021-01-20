454740d268a30ef5b54f9c66d7770f04f706e5dc
han910917 <han910917@163.com>
2021-01-20 17:52:04 +0800

    修改index的返回类型（由html转json）

--- user/src/main/java/com/han/user/controller/UserController.java
+++ user/src/main/java/com/han/user/controller/UserController.java
@@ -2,11 +2,13 @@ package com.han.user.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UserController {
 
     @RequestMapping("/index")
+    @ResponseBody
     public String hello() {
         //这边我们,默认是返到templates下的login.html
         return "index";

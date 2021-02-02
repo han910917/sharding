2ed56ee4642580f170a93e2c94e92f581e0c9494
han910917 <han910917@163.com>
2021-02-02 10:43:55 +0800

    修改静态文件302问题

--- user/src/main/java/com/han/user/config/UserSecurityConfig.java
+++ user/src/main/java/com/han/user/config/UserSecurityConfig.java
@@ -1,6 +1,5 @@
 package com.han.user.config;
 
-import com.han.user.common.MyPasswordEncoder;
 import com.han.user.filter.CheckCodeFilter;
 import com.han.user.handler.LoginFailureHandler;
 import com.han.user.handler.LoginSuccessHandler;
@@ -8,6 +7,7 @@ import com.han.user.provider.ComAuthenticationProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -48,5 +48,11 @@ public class UserSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
        auth.authenticationProvider(comAuthenticationProvider);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+        web.ignoring().antMatchers("/static/**");
     }
 }
